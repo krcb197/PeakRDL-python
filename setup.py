18de23bc42c2a62abcf4332b3d68655454cc5944
Keith Brady <34693973+krcb197@users.noreply.github.com>
2021-08-28 22:14:58 +0100

    Removed support for python 3.5 and 3.6

--- setup.py
+++ setup.py
@@ -31,8 +31,6 @@ setuptools.setup(
         "Development Status :: 3 - Alpha",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
