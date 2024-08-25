6192d0f8db9ce9daa3ff339da1edf747ebb514cb
krcb197 <34693973+krcb197@users.noreply.github.com>
2024-08-25 18:08:01 +0100

    Remove argparse from the sphinx build as it is not used anymore

--- docs/conf.py
+++ docs/conf.py
@@ -30,8 +30,7 @@ author = 'Keith Brady'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc',
-              'sphinx.ext.napoleon',
-              'sphinxarg.ext']
+              'sphinx.ext.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
