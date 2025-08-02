cdb2f3cab1a4e81c63cb4ac5e118d57d60366118
krcb197 <34693973+krcb197@users.noreply.github.com>
2025-08-02 17:38:59 +0100

    Include the has of an enum in the python class name to avoid issues with enums in XML files

--- src/peakrdl_python/class_names.py
+++ src/peakrdl_python/class_names.py
@@ -23,6 +23,7 @@ from systemrdl.node import RegNode
 from systemrdl.node import MemNode
 from systemrdl.rdltypes.user_enum import UserEnumMeta
 from .systemrdl_node_utility_functions import is_encoded_field
+from .systemrdl_node_hashes import enum_hash
 
 def __get_field_get_base_class_name(node: FieldNode, async_library_classes: bool) -> str:
     if not isinstance(node, FieldNode):
@@ -102,4 +103,8 @@ def fully_qualified_enum_type(field_enum: UserEnumMeta) -> str:
     """
     Returns the fully qualified class type name, for an enum
     """
-    return field_enum.get_scope_path('_') + '_' + field_enum.type_name
+    enum_hash_value = enum_hash(field_enum)
+    full_scope_path = field_enum.get_scope_path('_')
+    if enum_hash_value < 0:
+        return full_scope_path + '_' + field_enum.type_name + '_neg_' + hex(-enum_hash_value)
+    return full_scope_path + '_' + field_enum.type_name + hex(enum_hash_value)
