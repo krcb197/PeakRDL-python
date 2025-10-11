ded511ce7c026b107181fb6e953217627a4d1838
krcb197 <34693973+krcb197@users.noreply.github.com>
2025-10-11 09:31:52 +0100

    Update all the copyright messages to 2025

--- tests/unit_tests/test_export.py
+++ tests/unit_tests/test_export.py
@@ -1,4 +1,20 @@
 """
+peakrdl-python is a tool to generate Python Register Access Layer (RAL) from SystemRDL
+Copyright (C) 2021 - 2025
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 Test exporting this tests a couple of different things:
 1. calling the exporter
 2. tests some of the export options that would not other be checked with the integration tests get
