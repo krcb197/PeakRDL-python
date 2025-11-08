a2ff8f3a452dfaa4838e034bf89dd3874031d4a6
krcb197 <34693973+krcb197@users.noreply.github.com>
2025-11-08 17:39:20 +0000

    move int field test to a function to reduced code closes #234

---
+++ src/peakrdl_python/lib_test/utilities.py
@@ -0,0 +1,70 @@
+"""
+peakrdl-python is a tool to generate Python Register Access Layer (RAL) from SystemRDL
+Copyright (C) 2021 - 2025
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+This package It provide methods used by the tests
+"""
+from ..lib import Field
+
+def reverse_bits(value: int, number_bits: int) -> int:
+    """
+    reverse the order of bits in an integer
+
+    Args:
+        value: value to reverse
+        number_bits: number of bits used in the value
+
+    Returns:
+        reversed valued
+    """
+    result = 0
+    for i in range(number_bits):
+        if (value >> i) & 1:
+            result |= 1 << (number_bits - 1 - i)
+    return result
+
+def expected_reg_write_data(fut: Field,
+                            reg_base_value: int,
+                            field_value: int) -> int:
+    """
+    Test utility function to generate the expected value to be write to a register, given a field
+    update in the register
+
+    Args:
+        fut: Field being updated
+        reg_base_value: register value before the write
+        field_value: new field value
+
+    Returns:
+
+    """
+    # pylint:disable-next=protected-access
+    readable_reg = fut.parent_register._is_readable
+
+    if readable_reg:
+        expected_data = reg_base_value & fut.inverse_bitmask
+        if fut.msb == fut.high:
+            expected_data |= (fut.bitmask & (field_value << fut.low))
+        else:
+            expected_data |= (fut.bitmask & (reverse_bits(value=field_value,
+                                                          number_bits=fut.width) << fut.low))
+
+        return expected_data
+
+    # if the register is not readable, the value is simply written
+    if fut.msb == fut.high:
+        return field_value << fut.low
+    return reverse_bits(value=field_value, number_bits=fut.width) << fut.low
